5e4145d84320018fac234957b086908416d23ec0
AllenLu <allenluuu@foxmail.com>
2023-06-12 17:23:34 +0000

    frontend/sysy: 解决常量初始化列表问题

--- src/lib/frontend/var.cpp
+++ src/lib/frontend/var.cpp
@@ -75,6 +75,146 @@ SysyFrontend::ConstDecl_Struct *SysyFrontend::ConstDecl_func()
     return ret_ptr;
 }
 
+// array-init helpers
+std::vector<KoopaInitList> ZeroInit_List(std::vector<SysyFrontend::ConstExp_Struct *> &lengths, int index)
+{
+    std::vector<KoopaInitList> list;
+    if (index == lengths.size() - 1)
+    {
+        for (int i = 0; i < lengths.at(index)->value.GetImm(); i++)
+        {
+            list.push_back(KoopaInitList(0));
+        }
+    }
+    else
+    {
+        for (int i = 0; i < lengths.at(index)->value.GetImm(); i++)
+        {
+            list.push_back(KoopaInitList(ZeroInit_List(lengths, index + 1)));
+        }
+    }
+    return list;
+}
+
+bool allZero(int *arr, int start, int end)
+{
+    for (int i = start; i < end; i++)
+    {
+        if (arr[i] != 0)
+            return false;
+    }
+    return true;
+}
+
+KoopaInitList CreateInitList(std::vector<SysyFrontend::ConstExp_Struct *> &lengths, KoopaInitList raw_list)
+{
+    // preparing
+    int val_ptrs[lengths.size()];
+    int init_ptrs[lengths.size()];
+    for (int i = 0; i < lengths.size(); i++)
+    {
+        val_ptrs[i] = 0;
+        init_ptrs[i] = 0;
+    }
+    int level = 0;
+    KoopaInitList new_list = KoopaInitList(ZeroInit_List(lengths, 0));
+
+    std::string length = "";
+    for (int i = 0; i < lengths.size(); i++)
+    {
+        length += std::to_string(lengths.at(i)->value.GetImm()) + " ";
+    }
+    std::cout << "length: " << length << std::endl;
+
+    // initializing
+    while (init_ptrs[0] < lengths.at(0)->value.GetImm())
+    {
+        int val = 0;
+        // find val & place
+        std::vector<KoopaInitList> cur_scope = raw_list.initList;
+        for (int i = 0; i < level; i++)
+        {
+            cur_scope = cur_scope.at(val_ptrs[i]).initList;
+        }
+        while (level >= 0 && val_ptrs[level] >= cur_scope.size())
+        {
+            val_ptrs[level] = 0;
+            if (!allZero(init_ptrs, level, lengths.size()))
+            {
+                for (int i = level; i < lengths.size(); i++)
+                {
+                    init_ptrs[i] = 0;
+                }
+                init_ptrs[level - 1]++;
+            }
+            level--;
+            val_ptrs[level]++;
+            cur_scope = raw_list.initList;
+            for (int i = 0; i < level; i++)
+            {
+                cur_scope = cur_scope.at(val_ptrs[i]).initList;
+            }
+        }
+        if (level < 0)
+            break;
+
+        std::cout << std::endl;
+
+        KoopaInitList cur_val = cur_scope.at(val_ptrs[level]);
+        while (cur_val.GetInitListType() != "int")
+        {
+            level++;
+            cur_val = cur_val.initList.at(0);
+            if (!allZero(init_ptrs, level, lengths.size()))
+            {
+                for (int i = level; i < lengths.size(); i++)
+                {
+                    init_ptrs[i] = 0;
+                }
+                init_ptrs[level - 1]++;
+            }
+        }
+        val = cur_val.initInt;
+
+        std::cout << "val_ptr: ";
+        for (int i = 0; i < lengths.size(); i++)
+        {
+            std::cout << val_ptrs[i] << " ";
+        }
+        val_ptrs[level]++;
+
+        std::cout << std::endl;
+        std::cout << "index: ";
+        for (int i = 0; i < lengths.size(); i++)
+        {
+            std::cout << init_ptrs[i] << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "val: " << val << std::endl << std::endl;
+        // fill val
+        KoopaInitList* holder = &new_list;
+        for (int i = 0; i < lengths.size(); i++)
+        {
+            holder = &holder->initList.at(init_ptrs[i]);
+        }
+        holder->initInt = val;
+        // go next
+        int focus = lengths.size() - 1;
+        init_ptrs[focus]++;
+        while (focus >= 0 && init_ptrs[focus] >= lengths.at(focus)->value.GetImm())
+        {
+            init_ptrs[focus] = 0;
+            focus--;
+            init_ptrs[focus]++;
+        }
+        if (focus < 0)
+            break;
+    }
+
+    std::cout << new_list.GetInitString() << std::endl;
+    return new_list;
+}
+
 // ConstDef      ::= IDENT {"[" ConstExp "]"} "=" ConstInitVal;
 SysyFrontend::ConstDef_Struct *SysyFrontend::ConstDef_func(SysyFrontend::Type_Struct *Type)
 {
@@ -104,22 +244,23 @@ SysyFrontend::ConstDef_Struct *SysyFrontend::ConstDef_func(SysyFrontend::Type_St
             {
                 ret_ptr->ConstInitVal = ConstInitVal_func();
                 KoopaVarType type = KoopaVarType::KOOPA_INT32;
-                for (int i = ret_ptr->Lengths.size() -1; i >= 0; i--)
+                for (int i = ret_ptr->Lengths.size() - 1; i >= 0; i--)
                 {
                     type = KoopaVarType::ARRAY_Type(type, ret_ptr->Lengths[i]->value.GetImm());
                 }
-                koopaIR->NewVarWithInit(type, ir_name, ret_ptr->ConstInitVal->initList);
+                koopaIR->NewVarWithInit(type, ir_name, CreateInitList(ret_ptr->Lengths, ret_ptr->ConstInitVal->initList));
             }
             else
             {
                 KoopaVarType type = KoopaVarType::KOOPA_INT32;
-                for (int i = ret_ptr->Lengths.size() -1; i >= 0; i--)
+                for (int i = ret_ptr->Lengths.size() - 1; i >= 0; i--)
                 {
                     type = KoopaVarType::ARRAY_Type(type, ret_ptr->Lengths[i]->value.GetImm());
                 }
                 KoopaVar var = koopaIR->NewVar(type, ir_name);
                 koopaIR->AddAllocStatement(var);
-                ret_ptr->ConstInitVal = ConstInitVal_func(&var);
+                ret_ptr->ConstInitVal = ConstInitVal_func();
+                koopaIR->AddStoreStatement(var, CreateInitList(ret_ptr->Lengths, ret_ptr->ConstInitVal->initList));
             }
         }
         else
@@ -177,20 +318,24 @@ SysyFrontend::ConstInitVal_Struct *SysyFrontend::ConstInitVal_func(KoopaVar *rec
             }
         }
         RESERVED_func(); // '}'
-        KoopaInitList *initList = new KoopaInitList();
+        ret_ptr->initList.initListType = KoopaInitList::KOOPA_INIT_AGGREGATE;
         for (int i = 0; i < ret_ptr->subStructPointer.InitList->size(); i++)
         {
             ConstInitVal_Struct *initVal = ret_ptr->subStructPointer.InitList->at(i);
             if (initVal->type == SysyFrontend::ConstInitVal_Struct::ConstInitValType::ConstInitValType_Exp)
             {
-                initList->initList.push_back(initVal->value.GetImm());
+                if (!initVal->value.IsImm())
+                {
+                    std::cerr << "Unsupported: Initializing array with variables" << std::endl;
+                    exit(1);
+                }
+                ret_ptr->initList.initList.push_back(initVal->value.GetImm());
             }
             else if (initVal->type == SysyFrontend::ConstInitVal_Struct::ConstInitValType::ConstInitValType_InitList)
             {
-                initList->initList.push_back(initVal->initList);
+                ret_ptr->initList.initList.push_back(initVal->initList.initList);
             }
         }
-        koopaIR->AddStoreStatement(*receiver, *initList);
     }
     else
     {
@@ -261,7 +406,7 @@ SysyFrontend::VarDef_Struct *SysyFrontend::VarDef_func(SysyFrontend::Type_Struct
                     {
                         type = KoopaVarType::ARRAY_Type(type, ret_ptr->VarDefLeft->Lengths[i]->value.GetImm());
                     }
-                    koopaIR->NewVarWithInit(type, ir_name, ret_ptr->InitVal->initList);
+                    koopaIR->NewVarWithInit(type, ir_name, CreateInitList(ret_ptr->VarDefLeft->Lengths, ret_ptr->InitVal->initList));
                 }
                 else
                 {
@@ -305,7 +450,8 @@ SysyFrontend::VarDef_Struct *SysyFrontend::VarDef_func(SysyFrontend::Type_Struct
                     }
                     KoopaVar var = koopaIR->NewVar(type, ir_name);
                     koopaIR->AddAllocStatement(var);
-                    ret_ptr->InitVal = InitVal_func(&var);
+                    ret_ptr->InitVal = InitVal_func();
+                    koopaIR->AddStoreStatement(var, CreateInitList(ret_ptr->VarDefLeft->Lengths, ret_ptr->InitVal->initList));
                 }
                 else
                 {
@@ -403,21 +549,24 @@ SysyFrontend::InitVal_Struct *SysyFrontend::InitVal_func(KoopaVar *receiver)
             }
         }
         RESERVED_func(); // "}"
-        KoopaInitList *initList = new KoopaInitList();
-        initList->initListType = KoopaInitList::KOOPA_INIT_AGGREGATE;
+        ret_ptr->initList.initListType = KoopaInitList::KOOPA_INIT_AGGREGATE;
         for (int i = 0; i < ret_ptr->subStructPointer.InitList->size(); i++)
         {
             InitVal_Struct *initVal = ret_ptr->subStructPointer.InitList->at(i);
             if (initVal->type == SysyFrontend::InitVal_Struct::InitValType::InitValType_Exp)
             {
-                initList->initList.push_back(initVal->value.GetImm());
+                if (!initVal->value.IsImm())
+                {
+                    std::cerr << "Unsupported: Initializing array with variables" << std::endl;
+                    exit(1);
+                }
+                ret_ptr->initList.initList.push_back(initVal->value.GetImm());
             }
             else if (initVal->type == SysyFrontend::InitVal_Struct::InitValType::InitValType_InitList)
             {
-                initList->initList.push_back(initVal->initList);
+                ret_ptr->initList.initList.push_back(initVal->initList.initList);
             }
         }
-        koopaIR->AddStoreStatement(*receiver, *initList);
     }
     else
     {
